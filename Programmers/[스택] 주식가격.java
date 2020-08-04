0c28d28529c51a58e11665689491fed8edb90077
jinyand <jiiin97@naver.com>
2020-08-04 15:59:16 +0900

    Create [스택] 주식가격.java

---
+++ Programmers/[스택] 주식가격.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        for(int i = 0; i < prices.length; i++) {
+            while(!stack.empty() && prices[stack.peek()] > prices[i]) {
+                int tmp = stack.pop();
+                answer[tmp] = i - tmp;
+            }
+            stack.push(i);
+        }
+        
+        while(!stack.empty()) {
+            int tmp = stack.pop();
+            answer[tmp] = (prices.length - 1) - tmp;
+        }
+        
+        return answer;
+    }
+}
